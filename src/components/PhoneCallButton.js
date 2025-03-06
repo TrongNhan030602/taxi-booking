2c0f15a76136375852a585ea602f8a32c2899e3c
TrongNhan030602 <ntnhan030602@gmail.com>
2025-03-06 16:29:11 +0700

    "update: phone Text"

--- src/components/PhoneCallButton.js
+++ src/components/PhoneCallButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaPhoneAlt } from "react-icons/fa";
-import "../assets/css/PhoneCallButton.css"; // Import file CSS
+import "../assets/css/PhoneCallButton.css";
 
 const PhoneCallButton = ({ phoneNumber }) => {
   return (
@@ -11,14 +11,41 @@ const PhoneCallButton = ({ phoneNumber }) => {
         <div className="arrow-icon"></div>
       </div>
 
-      {/* Nút gọi điện */}
-      <a
-        href={`tel:${phoneNumber}`}
-        className="phone-call-button"
-      >
-        <FaPhoneAlt className="phone-icon" />
-        <span className="wave-1"></span>
-      </a>
+      {/* Nút gọi điện + Chữ cong */}
+      <div className="phone-wrapper">
+        {/* Nút gọi */}
+        <a
+          href={`tel:${phoneNumber}`}
+          className="phone-call-button"
+        >
+          <FaPhoneAlt className="phone-icon" />
+          <span className="wave-1"></span>
+        </a>
+
+        {/* Chữ cong nửa trên */}
+        <svg
+          className="curved-text-half"
+          viewBox="0 0 120 60"
+        >
+          <defs>
+            <path
+              id="halfCirclePath"
+              d="M10,45 A50,50 0 0,1 110,45"
+              fill="transparent"
+            />
+          </defs>
+          <text width="120">
+            <textPath
+              href="#halfCirclePath"
+              startOffset="50%"
+              textAnchor="middle"
+              dy="4"
+            >
+              Gọi ngay để đặt xe
+            </textPath>
+          </text>
+        </svg>
+      </div>
     </div>
   );
 };
